5bc8d858905b2313edf873f4dbdb80afc0cbdb3b
birajit-dev <birajitdebbarma@gmail.com>
2022-11-27 15:50:44 +0530

    updated port

--- index.js
+++ index.js
@@ -58,6 +58,6 @@ app.use('*', (req, res) => {
 
 
 // port where app is served
-app.listen(3000, () => {
-    console.log('The web server has started on port 3000');
+app.listen(4000, () => {
+    console.log('The web server has started on port 4000');
 });
