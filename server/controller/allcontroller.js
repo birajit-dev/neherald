9f96615148c6176254f20062773cbb59f93ab60b
birajit-dev <birajitdebbarma@gmail.com>
2022-11-28 22:37:41 +0530

    limited added for recent news

--- server/controller/allcontroller.js
+++ server/controller/allcontroller.js
@@ -84,7 +84,7 @@ const { assert } = require('console');
             try{
             let catName = req.params.cat;
             const categoryAll = await allNews.find({post_category:catName}).sort({news_id:-1}).lean();
-            const recentNewscat = await allNews.find({post_category:{$ne:catName}}).sort({news_id:-1}).lean();
+            const recentNewscat = await allNews.find({post_category:{$ne:catName}}).sort({news_id:-1}).limit('10').lean();
             const bnews = await breakingNews.find().sort({brnews_id:-1}).limit('5').lean();
 
             //const pk = await allKey.findOne({page_category:catName});
@@ -132,7 +132,7 @@ const { assert } = require('console');
 
         exports.topNewsPage = async(req, res) =>{
             const topheadlines = await allNews.find({ne_insight:'yes'}).sort({news_id:-1}).lean();
-            const recentNewscat = await allNews.find().sort({news_id:-1}).lean();
+            const recentNewscat = await allNews.find().sort({news_id:-1}).limit('10').lean();
             const bnews = await breakingNews.find().sort({brnews_id:-1}).limit('5').lean();
             res.render('topnews',{
                     pageTitle: 'Tripura Top News : Northeast Herald',
